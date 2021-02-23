dc2e8324b70ce85705dc06d65c3d91355f4a08e6
Mykhailo Sobko <m.t.sobko@gmail.com>
2021-02-23 21:38:43 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Twitter API Json Parser',
+      version='1.0',
+      description='Parse .json files from Twitter API',
+      author='Mykhailo-Taras Sobko',
+      author_email='mykhailo-taras.sobko@ucu.edu.ua',
+      url='',
+      py_modules = ['json_parser'],
+     )
+
